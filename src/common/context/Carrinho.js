f1ec1d1945ee89182062d24f478d3fa98f796572
Janaina Oliveira <janainaon1@gmail.com>
2022-08-23 11:05:43 -0300

    add qtde itens

--- src/common/context/Carrinho.js
+++ src/common/context/Carrinho.js
@@ -1,20 +1,28 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = 'CarrinhoContext';
 
 export const CarrinhoProvider = ({ children }) => {
   const [carrinho, setCarrinho] = useState([]);
+  const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
 
   return (
-    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+    <CarrinhoContext.Provider 
+      value={{ 
+        carrinho, 
+        setCarrinho,
+        quantidadeProdutos,
+        setQuantidadeProdutos
+      }}
+    >
       {children}
     </CarrinhoContext.Provider>
   )
 }
 
 export const useCarrinhoContext = () => {
-  const { carrinho, setCarrinho} = useContext(CarrinhoContext);
+  const { carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos } = useContext(CarrinhoContext);
 
   function mudarQuantidade(id, quantidade) {
     return carrinho.map(itemCarrinho => {
@@ -42,10 +50,18 @@ export const useCarrinhoContext = () => {
     setCarrinho(mudarQuantidade(id, -1))
   }
 
+  useEffect(() => {
+    const novaQuantidade = carrinho.reduce((contador, produto) =>
+      contador + produto.quantidade, 0);
+      setQuantidadeProdutos(novaQuantidade);
+  },[carrinho, setQuantidadeProdutos])
+
   return {
     carrinho,
     setCarrinho,
     addProduto,
-    removeProduto
+    removeProduto,
+    quantidadeProdutos,
+    setQuantidadeProdutos
   }
 }
