df93086a840b7636dbc3b7a460715e6f06d8a23d
Janaina Oliveira <janainaon1@gmail.com>
2022-08-23 10:47:06 -0300

    refactoring carrinho context

--- src/common/context/Carrinho.js
+++ src/common/context/Carrinho.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = 'CarrinhoContext';
@@ -11,4 +11,41 @@ export const CarrinhoProvider = ({ children }) => {
       {children}
     </CarrinhoContext.Provider>
   )
+}
+
+export const useCarrinhoContext = () => {
+  const { carrinho, setCarrinho} = useContext(CarrinhoContext);
+
+  function mudarQuantidade(id, quantidade) {
+    return carrinho.map(itemCarrinho => {
+      if (itemCarrinho.id === id && itemCarrinho.quantidade >= 0) itemCarrinho.quantidade += quantidade;
+      return itemCarrinho;
+    })
+  }
+
+  function addProduto(novoProduto) {
+    const temOProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id);
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto])
+    }
+    setCarrinho(mudarQuantidade(novoProduto.id, 1));
+  }
+
+  function removeProduto(id) {
+    const produto = carrinho.find(itemCarrinho => itemCarrinho.id === id);
+    const ehUltimo = produto.quantidade === 1;
+
+    if (ehUltimo) {
+      return setCarrinho(carrinhoAnterior => carrinhoAnterior.filter(itemCarrinho => itemCarrinho.id !== id))
+    }
+    setCarrinho(mudarQuantidade(id, -1))
+  }
+
+  return {
+    carrinho,
+    setCarrinho,
+    addProduto,
+    removeProduto
+  }
 }
--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,9 +1,9 @@
 import { Container } from './styles';
-import { memo, useContext } from 'react';
+import { memo } from 'react';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import { CarrinhoContext } from 'common/context/Carrinho';
+import { useCarrinhoContext } from 'common/context/Carrinho';
 
 
 function Produto({
@@ -13,20 +13,8 @@ function Produto({
   valor,
   unidade
 }) {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-
-  function addProduto(novoProduto) {
-    const temOProduto = carrinho.some(itemCarrinho => itemCarrinho.id === novoProduto.id);
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto])
-    }
-
-    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
-      if (itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
-      return itemDoCarrinho;
-    }))
-  }
+  const { carrinho, addProduto, removeProduto } = useCarrinhoContext();
+  const produtoNoCarrinho = carrinho.find(itemDoCarrinho => itemDoCarrinho.id === id);
 
   return (
       <Container>
@@ -42,10 +30,15 @@ function Produto({
         <div>
           <IconButton
             color="secondary"
+            onClick={() => removeProduto(id)}
+            disabled={!produtoNoCarrinho}
           >
             <RemoveIcon />
           </IconButton>
-          <IconButton onClick={() => addProduto({ nome, foto, id, valor })}>
+          {produtoNoCarrinho?.quantidade || 0}
+          <IconButton 
+            color="primary"
+            onClick={() => addProduto({ nome, foto, id, valor })}>
             <AddIcon />
           </IconButton>
         </div>
