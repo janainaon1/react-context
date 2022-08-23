caa4ec78328c3dd79fad3db8874ff050c4ace0ea
Janaina Oliveira <janainaon1@gmail.com>
2022-08-23 09:41:10 -0300

    add carrinho context

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { UsuarioProvider } from "common/context/Usuario";
+import { CarrinhoProvider } from "common/context/Carrinho";
 import Carrinho from "pages/Carrinho";
 import Feira from "pages/Feira";
 import Login from "pages/Login";
@@ -13,9 +14,11 @@ function Router() {
           <Route exact path="/">
             <Login />
           </Route>
-          <Route path="/feira">
-            <Feira />
-          </Route>
+          <CarrinhoProvider>
+            <Route path="/feira">
+              <Feira />
+            </Route>
+          </CarrinhoProvider>
         </UsuarioProvider>
         <Route path="/carrinho">
           <Carrinho />
